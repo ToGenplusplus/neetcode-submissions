613aa7d178093ed4657c726e43a6631426885ed8
Temi Owoaje <37200607+ToGenplusplus@users.noreply.github.com>
2026-08-31 10:22:48 -0400

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.py
@@ -0,0 +1,55 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # return self.reverseListRecursive(head)
+        return self.reverseListIterative(head)
+
+
+    def reverseListRecursive(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+            examples: head: [0,1,2,3]
+            base case
+                if head == None or Head.next == None 
+                    return Head - this becomes our new head
+
+                3 - return head
+                2.next = 3.next = 2 return 3(newhead)
+                1.next = 2.next = 1 return 3(newhead)
+                1.next = None
+        """
+        if not head or not head.next:
+            return head
+
+        newHead = self.reverseListRecursive(head.next)
+        head.next.next = head
+        head.next = None
+
+        return newHead
+
+    def reverseListIterative(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+            examples: head: [0,1,2,3]
+            prev= None
+            nxt = 0.next = 1
+            0.next = prev
+            prev = curr
+            curr = nxt
+            nxt = 1.next = 2
+            1.next = prev
+        """
+        prev = None
+        curr = head
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+
+        return prev
+        
+    
